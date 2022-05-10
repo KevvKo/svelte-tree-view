854abb9ae6bff5cb1f8236e81f058955820aecaf
KevvKo <kklein915@gmail.com>
2022-05-10 22:36:04 +0200

    small changes

--- src/App.svelte
+++ src/App.svelte
@@ -3,16 +3,16 @@
     import {mockData} from './mock.js';
 </script>
 
-<main class="mt-5 mx-5">
-    <nav class="flex bg-cyan-700 rounded-t-lg py-4 px-5 text-slate-100">
-        <h1 class="self-center text-lg font-medium" >Svelte-Tree-View</h1>
-        <a href="https://github.com/KevvKo/svelte-tree-view" class="self-center ml-auto cursor-pointer">
-            <img alt="Svelte" src="img/svelte-logo.png" class="h-8"/>
-        </a> 
-        <a href="https://github.com/KevvKo/svelte-tree-view" class="self-center cursor-pointer ml-10">
-            <img alt="Github" src="img/GitHub-Mark-Light-32px.png" />
-        </a> 
-    </nav>
+<nav class="mt-5 mx-5 flex bg-teal-700 rounded-t-lg py-4 px-5 text-slate-100">
+    <h1 class="self-center text-lg font-medium" >Svelte-Tree-View</h1>
+    <a href="https://svelte.dev" class="self-center ml-auto cursor-pointer">
+        <img alt="Svelte" src="img/svelte-logo.png" class="h-8"/>
+    </a> 
+    <a href="https://github.com/KevvKo/svelte-tree-view" class="self-center cursor-pointer ml-10">
+        <img alt="Github" src="img/GitHub-Mark-Light-32px.png" />
+    </a> 
+</nav>
+<main class="mt-5 mx-5 px-5">
     <SvelteTree data={mockData}></SvelteTree>
 </main>
 
