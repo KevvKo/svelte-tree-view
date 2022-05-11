17a85a8732551daa35f8d62519f0b24369d24e86
KevvKo <kklein915@gmail.com>
2022-05-11 23:16:10 +0200

    redesign navbar

--- src/App.svelte
+++ src/App.svelte
@@ -3,13 +3,13 @@
     import {mockData} from './mock.js';
 </script>
 
-<nav class="mt-5 mx-5 flex bg-teal-700 rounded-t-lg py-4 px-5 text-slate-100">
-    <h1 class="self-center text-lg font-medium" >Svelte-Tree-View</h1>
+<nav class="flex shadow px-10 py-5">
+    <h1 class="self-center text-lg font-extrabold" >Svelte-Tree-View</h1>
     <a href="https://svelte.dev" class="self-center ml-auto cursor-pointer">
         <img alt="Svelte" src="img/svelte-logo.png" class="h-8"/>
     </a> 
     <a href="https://github.com/KevvKo/svelte-tree-view" class="self-center cursor-pointer ml-10">
-        <img alt="Github" src="img/GitHub-Mark-Light-32px.png" />
+        <img alt="Github" src="img/GitHub-Mark-32px.png" />
     </a> 
 </nav>
 <main class="mt-5 mx-5 px-5">
