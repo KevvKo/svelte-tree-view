9bd81b0f3a1894d305ce753f3d14542c532e6f29
KevvKo <kklein915@gmail.com>
2022-05-13 17:27:15 +0200

    improve table layout

--- src/App.svelte
+++ src/App.svelte
@@ -14,29 +14,29 @@
 </nav>
 <main class="mt-5 mx-5 px-5">
     <h2 class="text-lg font-semibold text-slate-600 mb-5">Basic Usage</h2>
-    <section class="flex ml-5 mb-20">
+    <section class="flex mx-5 mb-20">
         <div class="w-1/2">
             <SvelteTree data={mockData}></SvelteTree>
         </div>
         <div class="w-1/2">
-            <code class="bg-slate-700 text-slate-300 w-full h-full block p-5">
+            <code class="bg-slate-700 text-slate-300 w-full h-full block p-5 rounded">
                 &lt;Svelte-Tree-View data=&#123;mockData&#125;&gt;&lt;/Svelte-Tree-View&gt;
             </code>
         </div>
     </section>
     <h2 class="text-lg font-semibold text-slate-600 mb-5">API</h2>
-    <section>
-        <table class="w-full ml-5">
-            <thead>
+    <section class="mx-5">
+        <table class="w-full table-auto">
+            <thead class="bg-gray-50">
                 <tr>
-                    <th class="w-1/2 text-left">Parameter</th>
-                    <th class="w-1/2 text-left">Description</th>
+                    <th class="p-4 w-1/2 text-left">Parameter</th>
+                    <th class="p-4 w-1/2 text-left">Description</th>
                 </tr>
             </thead>
-            <tbody class="divide-y divide-solid">
-                <tr>
-                    <td>data</td>
-                    <td>Object of child and parents to render as a tree</td>
+            <tbody>
+                <tr class="border-b-2 border-slate-200 hover:bg-gray-100 duration-200 transition-all">
+                    <td class="p-4">data</td>
+                    <td class="p-4">Object of child and parents to render as a tree</td>
                 </tr>
             </tbody>
         </table>
