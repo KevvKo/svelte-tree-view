4ec13034ec2b41a302946e8caa6fe82da437715b
KevvKo <kklein915@gmail.com>
2022-05-17 22:08:29 +0200

    add arrows for click events

--- src/components/TreeNode.svelte
+++ src/components/TreeNode.svelte
@@ -2,7 +2,7 @@
     export let node;
     export let indent = 0;
 
-    let open = true;
+    let open = false;
 
     function toggleOpen(){
         open = !open;
@@ -15,6 +15,15 @@
 </script>
 
 <li style="margin-left: {indent}px">
+    {#if open}
+        <svg on:click={handleClick} xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+        </svg>
+    {:else}
+        <svg on:click={handleClick} xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+        </svg>
+    {/if}
     <input type='checkbox'/>
     <span>{node.name}</span>
 
