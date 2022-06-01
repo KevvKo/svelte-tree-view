c64cbb1bbad72c23f5e9d72dcc3c4e0aed410645
KevvKo <kklein915@gmail.com>
2022-06-01 19:39:59 +0200

    update for exported components

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 // import App from './App.svelte';
 
-import SvelteTree from "./lib/src/components/SvelteTree.svelte";
-
 // const app = new App({
 // 	target: document.body,
 // });
 
 // export default app;
 
-export default SvelteTree;
+export { default as SvelteTree } from "./lib/src/components/SvelteTree.svelte";
+export { default as TreeBranch } from "./lib/src/components/TreeBranch.svelte";
+export { default as TreeNode } from "./lib/src/components/TreeNode.svelte";
